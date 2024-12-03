2689a5249e6ffd0b48a2f6223749d98d51e77a84
Aaron Berkhoff <aaronberkhoff@outlook.com>
2024-12-03 15:37:48 -0600

    Data handler construction

--- src/ML_main.py
+++ src/ML_main.py
@@ -1,4 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sklearn 
+from data_handler import DataHandler
 
+#load data
+directory = 'C:/Users/ajber/Desktop/College Classes/Fall_2024/Machine_Learning/project/ML_project/data'
+
+data_handler = DataHandler(directory)
+
+name_data = 'Position_2_Body_No_Noise'
+
+X_data = data_handler.data[name_data][:,0]
+Y_data = data_handler.data[name_data][:,0]
+Z_data = data_handler.data[name_data][:,0]
+
+
+pass 
