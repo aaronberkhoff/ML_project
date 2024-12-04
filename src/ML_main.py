584971e60a8318452676c225fdecf79dc51cd0fb
Jacob Fulhorst <jacobfulhorst@yahoo.com>
2024-12-03 19:47:51 -0600

    where I'm calling it for the night

--- src/ML_main.py
+++ src/ML_main.py
@@ -27,27 +27,68 @@ Z_data_noNoise = data_handler.data[name_data][:,0]
 time_between_measurements=10 #seconds
 time_data=np.arange(0,X_data.shape[0]*time_between_measurements,time_between_measurements)
 
-numSinusoidals=50
-period=5400
-
-time_data_featurized=fourier_featurize(time_data,numSinusoidals,period)
-# time_data_featurized=polynomial_featurize(time_data,100)
+phaseX=time_data[np.argmax(X_data_noNoise)]
+# phaseX=0
 
 
-# linreg=LinearRegression()
-linreg=Ridge()
+numSinusoidals=200
+period=6200
+polyDegree=5
+
+time_data_featurized=fourier_featurize(time_data,numSinusoidals,period,phaseX)
+# time_data_featurized=polynomial_featurize(time_data,polynomialDegree=polyDegree)
+
+
+linreg=LinearRegression()
+# linreg=Ridge()
 linreg.fit(time_data_featurized,X_data)
 
 testing_time_data=np.arange(0,X_data.shape[0]*time_between_measurements*2,time_between_measurements)
 testing_time_data_featurized=fourier_featurize(testing_time_data,numSinusoidals,period)
+# testing_time_data_featurized=polynomial_featurize(testing_time_data,polyDegree)
 
 
-predictions=linreg.predict(testing_time_data_featurized)
 
-plt.figure()
-plt.scatter(time_data,X_data)#-X_data_noNoise)
-plt.plot(testing_time_data,predictions,color='r')
+predictions=linreg.predict(fourier_featurize(np.linspace(0,max(time_data),150001),numSinusoidals,period,phaseX))
+
+# plt.figure()
+# plt.scatter(time_data,X_data)#-X_data_noNoise)
+# # plt.plot(time_data,predictions,color='r')
+# plt.plot(np.linspace(0,max(time_data),150001),predictions,color='r')
+# plt.show()
+
+
+prediction=linreg.predict(time_data_featurized)
+
+# plt.figure()
+# plt.scatter(time_data,X_data-X_data_noNoise)
+# # plt.plot(time_data,predictions,color='r')
+# plt.plot(time_data,prediction-X_data_noNoise,color='r')
+# plt.show()
+
+
+
+
+fig,axs=plt.subplots(2,1,figsize=(8,6),constrained_layout=True)
+
+axs[0].scatter(time_data,X_data,label='Observed Data',alpha=0.6)
+axs[0].plot(np.linspace(0,max(time_data),150001),predictions,color='r',label='Prediction')
+axs[0].set_title('Prediction vs Observed Data')
+axs[0].set_xlabel('Time')
+axs[0].set_ylabel('X Data')
+axs[0].legend()
+axs[0].grid(True)
+
+axs[1].scatter(time_data,X_data-X_data_noNoise,label='Noisy Data',alpha=0.6)
+axs[1].plot(time_data,prediction-X_data_noNoise,color='r',label='Prediction Residuals')
+axs[1].set_title('Prediction Residuals')
+axs[1].set_xlabel('Time')
+axs[1].set_ylabel('Residuals')
+axs[1].legend()
+axs[1].grid(True)
+
 plt.show()
+
 
 
 
--- src/feature_matrix.py
+++ src/feature_matrix.py
@@ -3,7 +3,7 @@
 #12-3-2024
 import numpy as np
 
-def fourier_featurize(x,numSinusoidals=20,period=2*np.pi):
+def fourier_featurize(x,numSinusoidals=20,period=2*np.pi,phaseShift=0):
     # if x is a vector of 5 input values and numSinusoidals=20
     # x_featurized will be 5x41, the first one is the intercept
     # period is the orbit period in seconds
@@ -11,8 +11,8 @@ def fourier_featurize(x,numSinusoidals=20,period=2*np.pi):
     x_featurized=np.zeros([x.shape[0],(numSinusoidals*2)+1])
     x_featurized[:,0]=1
     for i in range(numSinusoidals):
-        x_featurized[:,2*i+1]=np.cos((i+1)*coefficient*x)
-        x_featurized[:,2*i+2]=np.sin((i+1)*coefficient*x)
+        x_featurized[:,2*i+1]=np.cos((i+1)*coefficient*x-phaseShift)
+        x_featurized[:,2*i+2]=np.sin((i+1)*coefficient*x-phaseShift)
     return x_featurized
 
 def polynomial_featurize(x,polynomialDegree=5):
@@ -23,7 +23,7 @@ def polynomial_featurize(x,polynomialDegree=5):
         x_featurized[:,i]=x**i
     return x_featurized
 
-def combination_featurize(x,numSinusoidals=20,period=2*np.pi,polynomialDegree=5):
+def combination_featurize(x,numSinusoidals=20,period=2*np.pi,polynomialDegree=5,phaseShift=0):
     # if x is a vector of 5 input values and polynomialDegree=20
     # x_featurized will be 5x21, the first one is the intercept
     # period is the orbit period in seconds
@@ -32,7 +32,7 @@ def combination_featurize(x,numSinusoidals=20,period=2*np.pi,polynomialDegree=5)
     for i in range(polynomialDegree+1):
         x_featurized[:,i]=x**i
     for i in range(numSinusoidals):
-        x_featurized[:,2*i+polynomialDegree+1]=np.cos((i+1)*coefficient*x)
-        x_featurized[:,2*i+polynomialDegree+2]=np.sin((i+1)*coefficient*x)
+        x_featurized[:,2*i+polynomialDegree+1]=np.cos((i+1)*coefficient*x-phaseShift)
+        x_featurized[:,2*i+polynomialDegree+2]=np.sin((i+1)*coefficient*x-phaseShift)
     return x_featurized
 
